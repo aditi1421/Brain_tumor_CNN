544bf7ba8820b74c4285ef80b344fe7114758571
Aditi Kumar <71980543+aditi1421@users.noreply.github.com>
2023-06-01 17:24:10 +0530

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -1,16 +1,3 @@
-"""
-Download dataset from :
-
-https://lhncbc.nlm.nih.gov/LHC-publications/pubs/TuberculosisChestXrayImageDataSets.html
-
-1. Download the dataset folder and create two folder training set and test set
-in the parent dataste folder
-2. Move 30-40 image from both TB positive and TB Negative folder
-in the test set folder
-3. The labels of the images will be extracted from the folder name
-the image is present in.
-
-"""
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
